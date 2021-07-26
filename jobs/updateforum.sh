57337a55795c038974a7031e99c51c584b317a37
Viet Tran <viettn85@gmail.com>
2021-07-27 06:52:50 +0700

    Update time

--- jobs/updateforum.sh
+++ jobs/updateforum.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-cd /Users/viet_tran/Workplace/viettn85/KQL/KqlForum/
+cd /Users/viet_tran/Workplace/kql/KqlForum/
 nohup python3 src/watch/getForumUpdate.py > logs/updatef.log &
 # https://stackoverflow.com/questions/29338066/run-python-script-at-os-x-startup
