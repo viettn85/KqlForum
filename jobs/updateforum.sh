90f1c18b20a66a44a995861f5a393a3b0d10ac00
Viet Tran <viettn85@gmail.com>
2021-10-08 09:03:05 +0700

    Update new data

--- jobs/updateforum.sh
+++ jobs/updateforum.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
-cd /Users/viet_tran/Workplace/kql/KqlForum/
-nohup python3 src/watch/getForumUpdate.py > logs/updatef.log &
-# https://stackoverflow.com/questions/29338066/run-python-script-at-os-x-startup
+cd ~
+/usr/bin/python3 src/watch/getForumUpdate.py
